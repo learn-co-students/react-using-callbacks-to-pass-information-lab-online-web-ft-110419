1f4aba7c2a82c1737723c590fc7896bc12ecdcb6
C-Mike576 <m.calkins576@gmail.com>
2020-03-30 14:40:19 -0400

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -5,7 +5,7 @@ export default class Cell extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      color: this.props.color
+      color: this.props.selectedColor
     }
   }
   
